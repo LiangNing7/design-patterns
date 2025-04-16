a00ce2ef3ad3dafd48e0ff60074619928babf577
LiangNing <liangning2277@gmail.com>
2025-04-16 19:24:18 +0800

    feat: behavioral/iterator

---
+++ 02-behavioral/iterator/iterator_test.go
@@ -0,0 +1,20 @@
+package iterator
+
+import "fmt"
+
+func Example() {
+	// 创建具体聚合对象.
+	aggregate := NewConcreteAggregate([]string{"apple", "banana", "cherry", "date"})
+	// 获取迭代器.
+	iterator := aggregate.CreateIterator()
+
+	// 遍历元素并输出.
+	for iterator.HasNext() {
+		fmt.Println(iterator.Next())
+	}
+	// Output:
+	// apple
+	// banana
+	// cherry
+	// date
+}
